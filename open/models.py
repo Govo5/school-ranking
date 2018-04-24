c6e75580cdf91685e44096e34c3755a9b969703e
luritas <luritas.me@gmail.com>
2018-04-24 15:18:39 +0900

    developing class....

--- open/models.py
+++ open/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from school_ranking.settings import STATICFILES_DIRS
 
 
-class TransactedPrice:
+class TransactedApi:
     name = '국토부실거래가'
     service_key = os.environ.get('OPEN_API_SERVICE_KEY')
 
@@ -24,25 +24,72 @@ class TransactedPrice:
         return self.korean
 
     def 아파트매매(self):
-        return self.data['아파트매매']
+        return TransactionPrice(self.data['아파트매매'])
 
     def 연립다세대매매(self):
-        return self.data['연립다세대매매']
+        return TransactionPrice(self.data['연립다세대매매'])
 
     def 단독다가구매매(self):
-        return self.data['단독다가구매매']
+        return TransactionPrice(self.data['단독다가구매매'])
 
     def 오피스텔매매(self):
-        return self.data['오피스텔매매']
+        return TransactionPrice(self.data['오피스텔매매'])
 
     def 토지매매(self):
-        return self.data['토지매매']
+        return TransactionPrice(self.data['토지매매'])
 
     def 아파트전월세(self):
-        return self.data['아파트전월세']
+        return TransactionPrice(self.data['아파트전월세'])
 
     def 연립다세대전월세(self):
-        return self.data['연립다세대전월세']
+        return TransactionPrice(self.data['연립다세대전월세'])
 
     def 아파트매매상세(self):
-        return self.data['아파트매매상세']
+        return TransactionPrice(self.data['아파트매매상세'])
+
+
+class TransactionPrice:
+    def __init__(self, transaction_api):
+        self.version = transaction_api['version'],
+        self.korean = transaction_api['korean'],
+        self.english = transaction_api['english'],
+        self.operation = transaction_api['operation'],
+        self.description = transaction_api['description'],
+        self.url = transaction_api['url'],
+        self.parameters = transaction_api['parameters'],
+        """
+        "parameters": {
+            "LAWD_CD": {
+                "required": true,
+                "type": "integer",
+                "description": "지역코드",
+                "example": "11110"
+            },
+            "DEAL_YMD": {
+                "required": true,
+                "type": "integer",
+                "description": "지역코드",
+                "example": "11110"
+            }
+        }
+        """
+    def get_parameters_list(self):
+        """
+        필수 파라미터 list 를 리턴
+        :return:
+        """
+        pass
+
+    def set_parameters(self):
+        """
+        get_parameters_list 에서 받은 필수 파라미터에 값을 할당함
+        :return:
+        """
+        pass
+
+    def is_able(self):
+        """
+        통긴가능한 상태인지 점검
+        :return:
+        """
+        pass
--- open/views.py
+++ open/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import TransactedPrice
+from .models import TransactedApi
 
 
 # Create your views here.
 
 
 def index(request):
-    transaction_price = TransactedPrice()
-    return HttpResponse(transaction_price)
+    transaction_api = TransactedApi()
+    return HttpResponse(transaction_api)
